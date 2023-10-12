a4b16a06b60b00408aed195ca27b0de5246fbd16
Steven Whitfield <stevmwhitfield@gmail.com>
2023-10-12 14:01:41 -0400

    Configure Tailwind and base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,29 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      '3xl': ['8rem', '1'],
+      '2xl': ['4.5rem', '1'],
+      'xl': ['3rem', '1'],
+      'lg': ['1.5rem', '1'],
+      'base': ['1.25rem', '1.5'],
+      'sm': ['1rem', '1'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: 'rgb(246 247 248 / <alpha-value>)',
+          200: 'rgb(219 222 226 / <alpha-value>)',
+          300: 'rgb(194 201 206 / <alpha-value>)',
+          400: 'rgb(173 181 189 / <alpha-value>)',
+          500: 'rgb(142 151 163 / <alpha-value>)',
+          600: 'rgb(125 133 147 / <alpha-value>)',
+          700: 'rgb(112 118 133 / <alpha-value>)',
+          800: 'rgb(78 82 90 / <alpha-value>)',
+          900: 'rgb(50 52 57 / <alpha-value>)',
+        },
+      },
+    },
   },
   plugins: [],
 };
